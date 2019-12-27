f7d1618cc221b5518a078db8966bc1c6b142edb3
tumpa sarkar <adrita@tumpas-MacBook-Pro.local>
2019-12-26 20:29:28 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include_once "header.php";
+
+$number = 8;
+for($i=0; $i < $number; $i++){
+?>
+	<img src='https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg' class="puppy"/>
+<?php
+}
+include_once "footer.php";
+?>
