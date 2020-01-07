f2ef6869f2a07a6ee48eee664e6d3e587e89716e
tumpa sarkar <adrita@tumpas-MacBook-Pro.local>
2020-01-07 16:49:06 -0500

    Edited projects.php

--- projects.php
+++ projects.php
@@ -8,22 +8,7 @@ $sql = "SELECT id, name, link, description FROM Projects;";
 $result = $conn->query($sql);
 ?>
 
-<form action="projects.php" method="post">
-	<p>Id:</p>
-	<input type="text" name="my_id">
-	<p>Name:</p>
-	<input type="text" name="my_name">
-	<p>Link:</p>
-	<input type="text" name="my_link">
-	<p>Description:</p>
-	<input type="text" name="my_description">
-	<input type="submit" value="Submit">
-</form>
-
 <?php
-$sql1 = "INSERT INTO Projects (id, name, link, description) VALUES ($_POST["my_id"], $_POST["my_name"], $_POST["my_link"], $_POST["my_description"]);";
-
-$result1 = $conn->query($sql1);
 
 if ($result->num_rows > 0) {
     // output data of each row
@@ -37,9 +22,30 @@ if ($result->num_rows > 0) {
 //Form should have Name, Link, Desc
 //When submit is clicked it should call:
 //create_project('myproject [name]', 'mylink [link]', 'desc');
+
 ?>
+<form action="projects.php" method="post">
+	<p>Id:</p>
+	<input type="text" name="my_id">
+	<p>Name:</p>
+	<input type="text" name="my_name">
+	<p>Link:</p>
+	<input type="text" name="my_link">
+	<p>Description:</p>
+	<input type="text" name="my_description">
+	<input type="submit" value="Submit">
+<?php
+	create_project($_POST["my_id"], $_POST["my_name"], $_POST["my_link"], $_POST["my_description"]);
+?>
+</form>
 
 <?php
+
+function create_project(id, name, link, description) {
+	$sql1 = "INSERT INTO Projects (id, name, link, description) VALUES (id, name, link, description);";
+
+	$result1 = $conn->query($sql1);
+}
 
 $conn->close();
 include_once "footer.php";
